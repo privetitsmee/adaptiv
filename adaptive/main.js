2fdd833c1e7ce1e40814af3a73ea75099de26063
privetitsmee <110345831+privetitsmee@users.noreply.github.com>
2023-03-07 20:31:17 +0200

    Add files via upload

---
+++ adaptive/main.js
@@ -0,0 +1,16 @@
+
+
+let menuBtn = document.querySelector('.hamburger');
+let menu = document.querySelector('.mnu_top');
+menuBtn.addEventListener('click', function(){
+        menu.classList.toggle('active');
+	
+})
+
+
+
+
+
+
+
+
